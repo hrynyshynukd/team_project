56f0a2dbfe4bce3a913658f6582ad02d50664838
hrynyshynukd <viktoriia.r.hrynyshyn@ukd.edu.ua>
2026-03-10 22:35:52 +0200

    Create Log model for logging device information

---
+++ models/log.model.js
@@ -0,0 +1,35 @@
+const Sequelize = require("sequelize");
+const sequelize = require("../db/db");
+
+/* -------------------- Log model -------------------- */
+
+const Log = sequelize.define("logs", {
+  id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+
+  date: {
+    type: Sequelize.DATE,
+    allowNull: false,
+  },
+
+  deviceId: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+
+  category: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+
+  description: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+});
+
+module.exports = Log;
