273ff12537c0b4f9d7b72f57e5e8c9ad4aa4e83a
hrynyshynukd <viktoriia.r.hrynyshyn@ukd.edu.ua>
2026-03-10 22:39:16 +0200

    Add log transformation utility for view formatting

---
+++ utils/logs.transformer.js
@@ -0,0 +1,24 @@
+/* -------------------- Transform logs for view -------------------- */
+
+function transformLogs(logs, locale = "uk-UA") {
+  const options = {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  };
+
+  const formatter = new Intl.DateTimeFormat(locale, options);
+
+  return logs.map((log) => ({
+    id: log.id,
+    date: formatter.format(log.date),
+    deviceId: log.deviceId,
+    category: log.category,
+    description: log.description,
+  }));
+}
+
+module.exports = transformLogs;
